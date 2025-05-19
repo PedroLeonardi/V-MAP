6eb67b675b9487827021c5e72f66f0c7fbf3256d
davithekid <byjdrk.27@gmail.com>
2025-05-19 10:30:41 -0300

    feat: footer

---
+++ viewer/src/app/Components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+// social medias
+// links de ajuda
+// copyritht
+// 
+
+
+export default function Footer () {
+    return (
+        <>
+            <footer className="w-screen">
+                <div className="bg-gradient-to-t from-gray-950 to-gray-900
+                grid
+                lg:h-32
+                md:h-32
+                h-30 justify-items-center">
+                  <span className="flex items-center font-extrabold text-[15px] md:text-[20px]">&copy;Vmap 2025. Todos os direitos reservados.</span>
+                </div>
+            </footer>
+        </>
+    )
+}
