e3156fbc72c995bfdac1635ca8ea3859fa23d705
davithekid <byjdrk.27@gmail.com>
2025-05-22 21:43:06 -0300

    fix: verificação

--- src/viewer/src/app/alunoCadastro/page.jsx
+++ src/viewer/src/app/alunoCadastro/page.jsx
@@ -7,71 +7,107 @@ import { get } from 'lodash'
 
 export default function alunoCadastro() {
 
-    const [nome, setNome] = useState('');
-    const [senha, setSenha] = useState('');
-    const [cpf, setCPF] = useState('');
-    const [cpf_responsavel, setCPF_RESPONSAVEL] = useState('');
-    const [loading, setLoading] = useState(false)
+  const [nome, setNome] = useState('');
+  const [senha, setSenha] = useState('');
+  const [cpf, setCPF] = useState('');
+  const [cpf_responsavel, setCPF_RESPONSAVEL] = useState('');
+  const [loading, setLoading] = useState(false)
 
-    async function handleSubmit (e)  {
+  function formatarCPF(valor) {
+    // Remove tudo que não for número
+    valor = valor.replace(/\D/g, '');
 
-        e.preventDefault();
-        setLoading(true);
-        
-        let formsErrors = false;
+    // Aplica a máscara
+    valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+    valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
+    valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    return valor
+  }
 
-        if(formsErrors) return
+  // validação input nome
+  function validarNome(nome) {
+    // aqui só permito letras e espaços
+    return /^[A-Za-zÀ-ú\s]+$/.test(nome);
 
-        try {
+  }
 
-            const response = await axios.post('http://localhost:3001/aluno', {
-                nome: nome,
-                cpf_aluno: cpf,
-                senha: senha,
-                cpf_responsavel: cpf_responsavel
-            })
-            
-            console.log(response.data)
-            toast.success('Aluno cadastrado com sucesso.')
-        } catch (err) {
-            console.error(err)
-        }
+  function validarSenha(senha){
+    return senha.length >= 6;
+  }
 
+  async function handleSubmit(e) {
+
+    e.preventDefault();
+    setLoading(true);
+
+    let formsErrors = false;
+
+    if(!nome.trim() || !cpf.trim() || !senha.trim() || !cpf_responsavel.trim()){
+      toast.error('Preencha todos os campos');
     }
-    
-    return (
-        <>
+
+    if(!validarNome(nome)){
+      toast.error('Nome Inválido. Usuario deve conter letras e espaços');
+    }
+
+    if(!validarSenha(senha)){
+      toast.error('Senha deve conter pelo menos 6 caracteres');
+    }
+
+    if (formsErrors) return
+
+    try {
+
+      const response = await axios.post('http://localhost:3001/aluno', {
+        nome: nome,
+        cpf_aluno: cpf,
+        senha: senha,
+        cpf_responsavel: cpf_responsavel
+      })
+
+      console.log(response.data)
+      toast.success('Aluno cadastrado com sucesso.')
+    } catch (err) {
+      console.error(err)
+    }
+
+  }
+
+  return (
+    <>
 
       <div className="flex flex-col items-center justify-center min-h-screen">
         <h1 className="text-2xl font-bold mb-6">Crie sua conta</h1>
 
         <form
           onSubmit={handleSubmit}
-          
+
           className="flex flex-col gap-4 bg-gray-800 p-6 rounded-lg shadow-md w-80"
-          >
+        >
           <input
             className="border border-gray-300 rounded px-4 py-2"
             type="text"
             value={nome}
             onChange={e => setNome(e.target.value)}
             placeholder="Digite seu nome"
-            />
+          />
           <input
             className="border border-gray-300 rounded px-4 py-2"
             type="text"
             value={cpf}
-            onChange={e => setCPF(e.target.value)}
+            maxLength={14}
+            onChange={e => setCPF(formatarCPF(e.target.value))}
             placeholder="Digite seu CPF"
-            />
+          />
           <input
             className="border border-gray-300 rounded px-4 py-2"
             type="text"
             value={cpf_responsavel}
-            onChange={e => setCPF_RESPONSAVEL(e.target.value)}
+            maxLength={14}
+            onChange={e => setCPF_RESPONSAVEL(formatarCPF(e.target.value))}
             placeholder="Digite seu CPF"
-            />
-          
+          />
+
 
           <input
             className="border border-gray-300 rounded px-4 py-2"
@@ -79,12 +115,12 @@ export default function alunoCadastro() {
             value={senha}
             onChange={e => setSenha(e.target.value)}
             placeholder="Digite sua senha"
-            />
+          />
 
           <button
             type="submit"
             className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition-colors"
-            >
+          >
             Cadastrar
           </button>
         </form>
@@ -92,6 +128,6 @@ export default function alunoCadastro() {
 
     </>
   );
-  
-  
+
+
 }
