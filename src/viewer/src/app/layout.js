6c557baa2e918a89a2b7ea88a5b42854535cbd53
davithekid <byjdrk.27@gmail.com>
2025-05-25 11:14:09 -0300

    feat: estruturando layout para header e slidebar para se comportar nas telas solicitadas

--- src/viewer/src/app/layout.js
+++ src/viewer/src/app/layout.js
@@ -1,9 +1,9 @@
-
-"use client"
+"use client";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from 'sonner'
+import { Toaster } from "sonner";
 import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,19 +15,52 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex">
-        <Header/>
-        {children}
-        <Toaster/>
+
+        {/* 
+          estrutura diferente de layout para poder encaixar meus toast, 
+          header e sidebar...
+        */}
+
+
+        {/* header para telas pequenas */}
+        <div className="block lg:hidden">
+          <Header />
         </div>
+
+        {/* estrutura sidebar em telas grandes */}
+        <div className="flex">
+          {/* visivel apenas para telas grandes*/}
+          <div className="hidden lg:block">
+            <Sidebar />
+          </div>
+
+          {/* conteudo principal */}
+          <main className="flex-1">{children}</main>
+        </div>
+
+        <Toaster
+          position="top-right"
+          theme="dark"
+          richColors
+          closeButton
+          toastOptions={{
+            style: {
+              background: "#1f2937", // bg-gray-800
+              color: "#fff",
+              border: "1px solid #3b82f6",
+              borderRadius: "0.5rem",
+            },
+            className: "font-sans text-sm",
+            duration: 3000,
+          }}
+        />
+
       </body>
     </html>
   );
