b54b5f246e0700e074a04847b1e8b9af51ed7950
davithekid <byjdrk.27@gmail.com>
2025-05-27 17:38:05 -0300

    feat: add delete

--- src/router/responsavelRoute.js
+++ src/router/responsavelRoute.js
@@ -6,5 +6,7 @@ router.get('/', responsavel.getResponsavelAll);
 router.get("/:id", responsavel.getResponsavel);
 router.get('/cpf/:cpf', responsavel.getResponsavelByCPF);
 router.post('/', responsavel.createResponsavel);
+router.put('/:id', responsavel.updateResponsavel); 
+router.delete('/:id', responsavel.deleteResponsavel)
 
 export default router;
