df581a529f882be3f8406e663b1b7939e35ceccb
davithekid <byjdrk.27@gmail.com>
2025-05-27 17:51:41 -0300

    feat: hooks para contagem de usuarios

---
+++ src/viewer/src/app/Hooks/TotalAlunos.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function useFetchTotalAlunos(shouldFetch) {
+  const [totalAlunos, setTotalAlunos] = useState(0);
+
+  useEffect(() => {
+    const fetchTotalAlunos = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/aluno/total');
+        setTotalAlunos(response.data.total);
+      } catch (err) {
+        console.error("Erro ao buscar total de alunos", err);
+      }
+    };
+
+    if (shouldFetch) {
+      fetchTotalAlunos();
+    }
+  }, [shouldFetch]);
+
+  return totalAlunos;
+}
