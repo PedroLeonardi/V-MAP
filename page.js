fd1b339f43fd4840427a59f46ef81293d239d864
Nycolas <nycolasleandro@outlook.com>
2025-05-29 16:42:29 -0300

    Add files via upload
    Commit da pagina de login

---
+++ page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-900">
+      <h1 className="text-white text-6xl font-bold text-center">
+        Página Inicial (teste depois eu troco pela que eu fiz, acabou ficando no pc do senai, mals)
+      </h1>
+    </div>
+  );
+}
